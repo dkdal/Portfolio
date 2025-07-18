184e18be2e8b12c8b2f90673b7479cf519db69b0
Dhruv Kapoor <dhruv.kapoor@dal.ca>
2025-07-18 20:05:21 -0300

    Updated vue.config.js public path for deploymeny

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://github.com/dkdal/Portfolio/' : '/',
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
